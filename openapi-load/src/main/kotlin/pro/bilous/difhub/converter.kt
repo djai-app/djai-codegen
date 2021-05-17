5a6acf37baa32d8c54dd0a83174f064a4b32a0b7
Viktor Nahornyi <agentfan@gmail.com>
2021-05-17 16:26:55 +0300

    #36 added possibility of selection the datasetStatus from console in converter.kt

--- openapi-load/src/main/kotlin/pro/bilous/difhub/converter.kt
+++ openapi-load/src/main/kotlin/pro/bilous/difhub/converter.kt
@@ -1,14 +1,14 @@
 package pro.bilous.difhub
 
-import pro.bilous.difhub.config.DatasetStatus
 import pro.bilous.difhub.config.SystemSettings
 import pro.bilous.difhub.console.Console
 import pro.bilous.difhub.convert.DifHubToSwaggerConverter
 import pro.bilous.difhub.write.YamlWriter
 
 fun main() {
-	Console.select()
-	val systemSettings = SystemSettings(Console.system!!, DatasetStatus.APPROVED)
+	val console = Console()
+	console.select()
+	val systemSettings = SystemSettings(console.system!!, console.status!!)
 	DifHubToSwaggerConverter(systemSettings).convertAll().forEach {
 		YamlWriter(it.appName).write(it.openApi)
 	}
