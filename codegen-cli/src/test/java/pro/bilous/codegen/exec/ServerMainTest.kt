f060211081e79944aa900a4da76b27f5f27b9875
Vova Bilous <vova.bilous@hotmail.com>
2023-07-07 19:37:49 +0300

    rebuild test templates to follow flolive

--- codegen-cli/src/test/java/pro/bilous/codegen/exec/ServerMainTest.kt
+++ codegen-cli/src/test/java/pro/bilous/codegen/exec/ServerMainTest.kt
@@ -42,9 +42,9 @@ class ServerMainTest {
 	fun shouldGenerateUsingCustomTemplates() {
 		val execSettings = ExecSettings(
 			projectPath = "",
-			specFilePath = "/Users/vova/Projects/djai-codegen/codegen-cli/src/test/resources/pet-api.yml",
-			configFile = "/Users/vova/Projects/djai-codegen/codegen-cli/src/test/resources/pet-settings.yml",
-			templateDir = "/Users/vova/Projects/djai-codegen/codegen-cli/src/test/resources/test-templates"
+			specFilePath = "/Users/vova/Projects/PRO/djet-codegen/codegen-cli/src/test/resources/pet-api.yml",
+			configFile = "/Users/vova/Projects/PRO/djet-codegen/codegen-cli/src/test/resources/pet-settings.yml",
+			templateDir = "/Users/vova/Projects/PRO/djet-codegen/codegen-cli/src/test/resources/test-templates"
 		)
 		val outputStream = FileOutputStream(outputZip)
 		ServerMain().generate(outputStream, execSettings)
@@ -54,7 +54,7 @@ class ServerMainTest {
 
 	private fun unzipProject() {
 		ProcessBuilder()
-			.command("rm", "-rf", "/Users/vova/Projects/test-results/project-flo//")
+			.command("rm", "-rf", "/Users/vova/Projects/test-results/project-flo/")
 			.redirectError(ProcessBuilder.Redirect.INHERIT)
 			.redirectOutput(ProcessBuilder.Redirect.INHERIT)
 			.start()
